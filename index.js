b0ebdbb2bb2a9f2a25a245deb70e6b78051875ec
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-19 05:07:23 +0430

    started working on new module (unnamed for now)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// 
+var jalaali = require('jalaali-js');
+
+var d = jalaali.toGregorian(1380, 01, 01); // "1380/01/01"
+var date = new Date(d.gy, d.gm - 1, d.gd);
+
+var startDeven = (date.getFullYear()*10000) + ((date.getMonth()+1)*100) + date.getDate();
