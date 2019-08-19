70aac852aa8c93a5f4e68c21f9128be2899d6fd5
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-19 21:05:40 +0430

    assigned main module with the task of exporting final files   task progress:     done with two subunit of work

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var jalaali = require('jalaali-js');
+const fs = require('fs');
+const { promisify } = require('util');
+const jalaali = require('jalaali-js');
 
-var d = jalaali.toGregorian(1380, 01, 01); // "1380/01/01"
-var date = new Date(d.gy, d.gm - 1, d.gd);
+const d = jalaali.toGregorian(1380, 01, 01); // "1380/01/01"
+const date = new Date(d.gy, d.gm - 1, d.gd);
+const startDeven = (date.getFullYear()*10000) + ((date.getMonth()+1)*100) + date.getDate();
 
-var startDeven = (date.getFullYear()*10000) + ((date.getMonth()+1)*100) + date.getDate();
+const readFile = promisify(fs.readFile);
+const parse = promisify( require('csv-parse') );
+
+(async function () {
+	const csvStr = await readFile('./state/SelectedInstruments.csv', 'utf8');
+	const selectedInstruments = await parse(csvStr);
+	
+	selectedInstruments.forEach(v => {
+		
+	});
+})()
+
+
+
+
