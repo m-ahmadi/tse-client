9eb51b9a6302a589ae8a8e953fc1fa31ab85172a
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-19 06:09:15 +0430

    added new folder for data structures: struct added new struct: ClosingPriceRow.js   experimental name

---
+++ struct/ClosingPriceRow.js
@@ -0,0 +1,21 @@
+class ClosingPriceRow() {
+	constructor(_row='') {
+		const row: _row.split(',');
+		
+		if (row.length !== 11) throw new TypeError('Invalid ClosingPriceRow data!');
+		
+		this.InsCode        = row[0];
+		this.DEven          = row[1];
+		this.PClosing       = row[2];
+		this.PDrCotVal      = row[3];
+		this.ZTotTran       = row[4];
+		this.QTotTran5J     = row[5];
+		this.QTotCap        = row[6];
+		this.PriceMin       = row[7];
+		this.PriceMax       = row[8];
+		this.PriceYesterday = row[9];
+		this.PriceFirst     = row[10];
+	}
+}
+
+module.exports = ClosingPriceRow;
