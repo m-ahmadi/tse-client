074dc5fc80b11a32cabfff35ec56ac323a64bf5e
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-19 20:36:39 +0430

    added new structs:   Instrument   Share

---
+++ struct/Instrument.js
@@ -0,0 +1,31 @@
+// orig: InstrumentInfo
+
+class Instrument {
+	constructor(_row='') {
+		const row: _row.split(',');
+		
+		if (row.length !== 17) throw new TypeError('Invalid Instrument data!');
+
+		// unspecified one are string
+		this.InsCode      = row[0];  // int64 (long)
+		this.InstrumentID = row[1];
+		this.LatinSymbol  = row[2];
+		this.LatinName    = row[3];
+		this.CompanyCode  = row[4];
+		this.Symbol       = row[5];
+		this.Name         = row[6];
+		this.CIsin        = row[7];
+		this.DEven        = row[8];  // int32 (int)
+		this.Flow         = row[9];  // byte
+		this.LSoc30       = row[10];
+		this.CGdSVal      = row[11];
+		this.CGrValCot    = row[12];
+		this.YMarNSC      = row[13];
+		this.CComVal      = row[14];
+		this.CSecVal      = row[15];
+		this.CSoSecVal    = row[16];
+		this.YVal         = row[17];
+	}
+}
+
+module.exports = Instrument;
