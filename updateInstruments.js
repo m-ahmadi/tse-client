33fdcd61728b5f94ab5c2e11ec0429fe2afadb79
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-19 20:47:23 +0430

    module: updateInstruments.js   done with basic unit of work

--- updateInstruments.js
+++ updateInstruments.js
@@ -1,1 +1,29 @@
-const request = require('lib/request');
+const rq = require('./lib/request');
+const xmljs = require('xml-js');
+const fs = require('fs');
+const u = require('util-ma');
+
+const writeFile = require('util').promisify(fs.writeFile);
+const deven = '20010321';
+
+
+(async function getData() {
+	const axiosRes = await rq.InstrumentAndShare(deven).catch(console.log);
+	const response = xmljs.xml2js(axiosRes.data);
+	const data = response.elements[0].elements[0].elements[0].elements[0].elements[0].text;
+	
+	let instruments = data.split("@")[0];
+	let shares      = data.split("@")[1];
+	
+	if (!u.isEmptyStr(instruments) && instruments !== '*') {
+		writeFile('./data/instruments.csv', instruments.replace(/;/g, '\n').slice(0, -1) );
+	} else {
+		throw new Error('Invalid Instruments data!');
+	}
+	
+	if ( !u.isEmptyStr(shares) ) {
+		writeFile('./data/shares.csv', shares.replace(/;/g, '\n').slice(0, -1) );
+	} else {
+		throw new Error('Invalid Shares data!');
+	}
+})()
