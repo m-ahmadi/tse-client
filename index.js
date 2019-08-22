3914983c88180e7a8ae0ae597128122bf4d837d7
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-22 12:32:34 +0430

    module: main   progress: 2/10   reading closing prices of selected intruments

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const { promisify } = require('util');
 const jalaali = require('jalaali-js');
 
-const d = jalaali.toGregorian(1380, 01, 01); // "1380/01/01"
+const ClosingPriceRow = require('./struct/ClosingPriceRow');
+
+const j = "1380/01/01".split('/').map(v => parseInt(v));
+const d = jalaali.toGregorian(j[0], j[1], j[2]);
 const date = new Date(d.gy, d.gm - 1, d.gd);
 const startDeven = (date.getFullYear()*10000) + ((date.getMonth()+1)*100) + date.getDate();
 
@@ -12,9 +15,12 @@ const readFile = promisify(fs.readFile);
 	const csvStr = await readFile('./state/SelectedInstruments.csv', 'utf8');
 	let selectedInstruments = csvStr.slice(0, -1).split('\n');
 	
-	selectedInstruments.forEach(v => {
-		
-	});
+	let closingPrices = {};
+	for (v of selectedInstruments) {
+		const str = await readFile(`./data/${v}.csv`, 'utf8');
+		closingPrices[v] = str.split('\n').map( v => new ClosingPriceRow(v) );
+	}
+	
 })()
 
 
