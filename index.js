02730cc81f0f84b4af48674a76d20c585cffb346
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-08-23 04:32:09 +0430

    module: main   progress: 3/10

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const { promisify } = require('util');
 const jalaali = require('jalaali-js');
 
+const getSelectedInstruments = require('./lib/getSelectedInstruments');
 const ClosingPriceRow = require('./struct/ClosingPriceRow');
+const ColumnConfig = require('./struct/ColumnConfig');
 
 const j = "1380/01/01".split('/').map(v => parseInt(v));
 const d = jalaali.toGregorian(j[0], j[1], j[2]);
@@ -12,17 +14,32 @@ const startDeven = (date.getFullYear()*10000) + ((date.getMonth()+1)*100) + date
 const readFile = promisify(fs.readFile);
 
 (async function () {
-	const csvStr = await readFile('./state/SelectedInstruments.csv', 'utf8');
-	let selectedInstruments = csvStr.slice(0, -1).split('\n');
+	let selectedInstruments = await getSelectedInstruments();
 	
-	let closingPrices = {};
+	let insCosingPrices = {};
 	for (v of selectedInstruments) {
 		const str = await readFile(`./data/${v}.csv`, 'utf8');
-		closingPrices[v] = str.split('\n').map( v => new ClosingPriceRow(v) );
+		insCosingPrices[v] = str.split('\n').map( v => new ClosingPriceRow(v) );
 	}
 	
+	// if (instrument.YMarNSC != "ID")
+	const colstr = await readFile('./state/Columns.csv', 'utf8');
+	let columns = colstr.slice(0, -1).split('\n').map( v => new ColumnConfig(v) );
+	let headerRow = '';
+	for (column of columns) {
+		headerRow += column.Header + ',';
+	}
+	headerRow.slice(0, -1);
+	headerRow += '\n';
+	
+	let files = selectedInstruments
+		.map(insCode => insCosingPrices[insCode])
+		.map(closingPrice => {
+			let str = '';
+			for (column of columns) {
+				str += column.CompanyCode
+			}
+		});
+	
+	var x = n;
 })()
-
-
-
-
