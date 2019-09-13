bf577b4b97da065217931ff5531587c0abf002dc
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-09-13 19:01:31 +0430

    not deleting existing settings.cachDir postinstall scripts/postinstall:   don't delete cacheDir if it exists   cleanup

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,21 +1,6 @@
 const fs = require('fs');
-const { join } = require('path');
 const { cacheDir } = require('../defaultSettings');
 
-if ( fs.existsSync(cacheDir) ) rmdirRecursive(cacheDir);
-
-fs.mkdirSync(cacheDir);
-
-function rmdirRecursive(dir) {
-  if ( fs.existsSync(dir) ) {
-    fs.readdirSync(dir).forEach(file => {
-      const curPath = join(dir, file);
-      if ( fs.lstatSync(curPath).isDirectory() ) {
-        rmdirRecursive(curPath);
-      } else {
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(dir);
-  }
+if ( !fs.existsSync(cacheDir) ) {
+	fs.mkdirSync(cacheDir);
 }
