bb383493f2d38da099a3b2434f58092fade565cd
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-09-27 20:15:52 +0330

    fixed a typo that caused update & export errors

--- updateClosingPrices.js
+++ updateClosingPrices.js
@@ -9,28 +9,28 @@ const getSelectedInstruments = require('./lib/getSelectedInstruments');
 const Instrument = require('./struct/Instrument');
 
 module.exports = async function () {
-	const selectedInstruments = await getSelectedInstruments(true);
-	
-	let insCodes = "";
-	for (instrument of selectedInstruments) {
-		insCodes += instrument.InsCode + ',';
-		// insCodes += instrument.DEven + ',';
-		insCodes += '20010321' + ',';
-		insCodes += instrument.YMarNSC === 'NO' ? 0 : 1;
-		insCodes += ';';
-	}
-	insCodes = insCodes.slice(0, -1);
-	
-	const axiosRes = await rq.ClosingPrices(insCodes).catch(console.log);
-	let data = axiosRes.data;
-	
-	if (data !== '') {
-		data = data.split('@').map( v => v.replace(/;/g, '\n') );
-		const writes = selectedInstruments.map( (v, i) => [ v.insCode, data[i] ] );
-		for (write of writes) {
-			await writeFile(`./data/${write[0]}.csv`, write[1]);
-		}
-	} else {
-		throw new Error('Invalid ClosingPrice data!');
-	}
+  const selectedInstruments = await getSelectedInstruments(true);
+  
+  let insCodes = "";
+  for (instrument of selectedInstruments) {
+    insCodes += instrument.InsCode + ',';
+    // insCodes += instrument.DEven + ',';
+    insCodes += '20010321' + ',';
+    insCodes += instrument.YMarNSC === 'NO' ? 0 : 1;
+    insCodes += ';';
+  }
+  insCodes = insCodes.slice(0, -1);
+  
+  const axiosRes = await rq.ClosingPrices(insCodes).catch(console.log);
+  let data = axiosRes.data;
+  
+  if (data !== '') {
+    data = data.split('@').map( v => v.replace(/;/g, '\n') );
+    const writes = selectedInstruments.map( (v, i) => [ v.InsCode, data[i] ] );
+    for (write of writes) {
+      await writeFile(`./data/${write[0]}.csv`, write[1]);
+    }
+  } else {
+    throw new Error('Invalid ClosingPrice data!');
+  }
 };
