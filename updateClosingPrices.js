f63d0af824f7765e27d42c81bd0edb20a0480e9e
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-09-29 17:04:12 +0330

    enhanced updateClosingPrices.js requesting only the missing data for the existing files appending new data to the existing files optimized to read the existing files only once creating new files for non-existent instruments guards against failed requests and bad server responses outputing logs and aborting on error situations

--- updateClosingPrices.js
+++ updateClosingPrices.js
@@ -1,36 +1,70 @@
 const fs = require('fs');
+const { join } = require('path');
 const { promisify } = require('util');
+const { isArr, isObj, isStr } = require('util-ma');
+const Big = require('big.js');
 
 const writeFile = promisify(fs.writeFile);
-const readFile = promisify(fs.readFile);
+const access = promisify(fs.access);
+const log = console.log;
 
+require('./lib/colors');
+const settings = require('./lib/settings');
 const rq = require('./lib/request.v2');
 const getSelectedInstruments = require('./lib/getSelectedInstruments');
+const readFileIntoArray = require('./lib/readFileIntoArray');
 const Instrument = require('./struct/Instrument');
+const ClosingPrice = require('./struct/ClosingPrice');
+
+const startDeven = '20010321';
 
 module.exports = async function () {
-  const selectedInstruments = await getSelectedInstruments(true);
+  const cacheDir = await settings.get('cacheDir');
+  const selection = await getSelectedInstruments(true);
+  let error;
+  const { data: res } = await rq.LastPossibleDeven().catch(err => error = err);
+  if (error)                        { log('Failed request: '.redBold          +'LastPossibleDeven: '+error.code.red+' \nOperation aborted.'.red); return; }
+  if ( !/^\d{8};\d{8}$/.test(res) ) { log('Invalid server response: '.redBold +'LastPossibleDeven'                 +' \nOperation aborted.'.red); return; }
+  const lastPossibleDeven = res.split(';')[0] || res.split(';')[1];
   
-  let insCodes = "";
-  for (instrument of selectedInstruments) {
-    insCodes += instrument.InsCode + ',';
-    // insCodes += instrument.DEven + ',';
-    insCodes += '20010321' + ',';
-    insCodes += instrument.YMarNSC === 'NO' ? 0 : 1;
-    insCodes += ';';
-  }
-  insCodes = insCodes.slice(0, -1);
-  
-  const axiosRes = await rq.ClosingPrices(insCodes).catch(console.log);
-  let data = axiosRes.data;
-  
-  if (data !== '') {
-    data = data.split('@').map( v => v.replace(/;/g, '\n') );
-    const writes = selectedInstruments.map( (v, i) => [ v.InsCode, data[i] ] );
-    for (write of writes) {
-      await writeFile(`./data/${write[0]}.csv`, write[1]);
+  const validSelection = {};
+  let insCodes = [];
+  for (instrument of selection) {
+    const insCode = instrument.InsCode;
+    const market = instrument.YMarNSC === 'NO' ? 0 : 1;
+    const filePath = join(cacheDir, `${insCode}.csv`);
+    if ( await pathExists(filePath) ) {
+      const content = await readFileIntoArray(filePath);
+      const lastRow = new ClosingPrice( content[content.length-1] );
+      if ( Big(lastPossibleDeven).gt(lastRow.DEven) ) {
+        insCodes.push( [insCode, lastRow.DEven, market] );
+        validSelection[insCode] = { path: filePath, oldContent: content };
+      }
+    } else {
+      insCodes.push( [insCode, startDeven, market] );
+      validSelection[insCode] = { path: filePath };
     }
-  } else {
-    throw new Error('Invalid ClosingPrice data!');
   }
+  insCodes = insCodes.map(i => i.join(',')).join(';');
+  
+  let { data } = await rq.ClosingPrices(insCodes).catch(err => error = err);
+  if (error)                                  { log('Failed request: '.redBold          +'ClosingPrices: '+error.code.red+' \nOperation aborted.'.red); return; }
+  if ( !/^[\d\.,;]*@?[\d\.,;]*$/.test(data) ) { log('Invalid server response: '.redBold +'ClosingPrices'                 +' \nOperation aborted.'.red); return; }
+  if (data === '') return;
+  data = data.split('@');
+  
+  const writes = Object.keys(validSelection).map((k, i) => {
+    const { path, oldContent } = validSelection[k];
+    const newContent = data[i].split(';');
+    const content = oldContent ? oldContent.concat(newContent) : newContent;
+    return [path, content];
+  });
+  
+  for (write of writes) await writeFile(write[0], write[1]);
 };
+
+async function pathExists(path) {
+  let res = true;
+  await access(path).catch(err => res = false);
+  return res;
+}
