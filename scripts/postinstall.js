d78a1404b1ea51bbb205b259e0ef1af17e084ad8
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-09-30 19:35:10 +0330

    ignored settings.json and creating it postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,43 @@
 const fs = require('fs');
 
+const defaultSettings = {
+  cacheDir: 'data',
+  lastInstrumentUpdate: 'never',
+  selectedInstruments: [],
+  selectedColumns: [],
+  selectedExport: {},
+  defaultColumns: [
+    2, '<TICKER>',
+    4, '<DTYYYYMMDD>',
+    6, '<OPEN>',
+    7, '<HIGH>',
+    8, '<LOW>',
+    10, '<CLOSE>',
+    12, '<VOL>',
+    11, '<PRICE>',
+    13, '<COUNT>',
+    14, '<YESTERDAY>'
+  ],
+  defaultExport: {
+    fileName: 4,
+    fileExtension: 'csv',
+    delimiter: ',',
+    adjustPrices: 0,
+    encoding: 'utf8',
+    daysWithoutTrade: false,
+    startDate: '1380/01/01',
+    showHeaders: true,
+    outDir: '.'
+  }
+};
+
 (async function () {
-  const cacheDir = await require('../lib/settings').get('cacheDir');
+  const filePath = './settings.json';
+  if ( !fs.existsSync(filePath) ) {
+    fs.writeFileSync( filePath, JSON.stringify(defaultSettings, null, 2) );
+  }
   
+  const cacheDir = await require('../lib/settings').get('cacheDir');
   if ( !fs.existsSync(cacheDir) ) {
     fs.mkdirSync(cacheDir);
   }
