bb9b6c8bc6763e70da0dd6d8be5c4eb5729a25f4
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-03-14 18:59:38 +0330

    renamed column arrs & placed outside Column struct

--- tse.js
+++ tse.js
@@ -63,14 +63,14 @@ class ClosingPrice {
 		this.PriceFirst     = row[10]; // open
 	}
 }
+const cols  = ['CompanyCode', 'LatinName', 'Symbol', 'Name', 'Date', 'ShamsiDate', 'PriceFirst', 'PriceMax', 'PriceMin', 'LastPrice', 'ClosingPrice', 'Price', 'Volume', 'Count', 'PriceYesterday'];
+const colsFa = ['کد شرکت', 'نام لاتین', 'نماد', 'نام', 'تاریخ میلادی', 'تاریخ شمسی', 'اولین قیمت', 'بیشترین قیمت', 'کمترین قیمت', 'آخرین قیمت', 'قیمت پایانی', 'ارزش', 'حجم', 'تعداد معاملات', 'قیمت دیروز'];
 class Column {
 	constructor(row=[]) {	
 		const len = row.length;
 		if (len > 2 || len < 1) throw new Error('Invalid Column data!');
-		const names  = ['CompanyCode', 'LatinName', 'Symbol', 'Name', 'Date', 'ShamsiDate', 'PriceFirst', 'PriceMax', 'PriceMin', 'LastPrice', 'ClosingPrice', 'Price', 'Volume', 'Count', 'PriceYesterday'];
-		const fnames = ['کد شرکت', 'نام لاتین', 'نماد', 'نام', 'تاریخ میلادی', 'تاریخ شمسی', 'اولین قیمت', 'بیشترین قیمت', 'کمترین قیمت', 'آخرین قیمت', 'قیمت پایانی', 'ارزش', 'حجم', 'تعداد معاملات', 'قیمت دیروز'];
-		this.name   = names[ row[0] ];
-		this.fname  = fnames[ row[0] ];
+		this.name   = cols[ row[0] ];
+		this.fname  = colsFa[ row[0] ];
 		this.header = row[1];
 	}
 }
