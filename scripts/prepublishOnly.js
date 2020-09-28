5b0ae6b8781e862f83f6e955f655417360ed7136
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-09-29 02:18:58 +0330

    corrected terser options in prepublishOnly.js

--- scripts/prepublishOnly.js
+++ scripts/prepublishOnly.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { execSync }  = require('child_process');
 process.env.path += require('path').delimiter + './node_modules/.bin';
 
-execSync('terser node_modules/big.js/big.js -o dep1.js -c -m --mangle-props');
+execSync('terser node_modules/big.js/big.js -o dep1.js -c -m');
 
 fs.writeFileSync('x.js', "module.exports = require('jalaali-js');");
 execSync('browserify x.js -s jalaali | terser -c -m -o dep2.js');
@@ -10,7 +10,7 @@ execSync('browserify x.js -s jalaali | terser -c -m -o dep2.js');
 fs.copyFileSync('node_modules/localforage/dist/localforage.min.js', 'dep3.js');
 
 fs.copyFileSync('tse.js', 'dist/tse.js');
-execSync('terser tse.js -o dist/tse.min.js -c -m --mangle-props');
+execSync('terser tse.js -o dist/tse.min.js -c -m');
 fs.writeFileSync('dist/tse.bundle.min.js', ['dep1.js','dep2.js','dep3.js','dist/tse.min.js'].map(i=>fs.readFileSync(i,'utf8')).join('\n'));
 
 ['dep1.js', 'dep2.js', 'dep3.js', 'x.js'].forEach(fs.unlinkSync);
