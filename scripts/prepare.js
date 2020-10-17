3858dc4498c151a6506b0c5999bed16c020600e5
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-10-18 01:43:25 +0330

    added `pako` to the browser bundle
    package.json:        added pako as dependency
    scripts/prepare.js:  included pako in generated bundle

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -8,10 +8,11 @@ fs.writeFileSync('x.js', "module.exports = require('jalaali-js');");
 execSync('browserify x.js -s jalaali | terser -c -m -o dep2.js');
 
 fs.copyFileSync('node_modules/localforage/dist/localforage.min.js', 'dep3.js');
+fs.copyFileSync('node_modules/pako/dist/pako.min.js', 'dep4.js');
 
 if (!fs.existsSync('dist')) fs.mkdirSync('dist');
 fs.copyFileSync('tse.js', 'dist/tse.js');
 execSync('terser tse.js -o dist/tse.min.js -c -m');
-fs.writeFileSync('dist/tse.bundle.min.js', ['dep1.js','dep2.js','dep3.js','dist/tse.min.js'].map(i=>fs.readFileSync(i,'utf8')).join('\n'));
+fs.writeFileSync('dist/tse.bundle.min.js', ['dep1.js','dep2.js','dep3.js','dep4.js','dist/tse.min.js'].map(i=>fs.readFileSync(i,'utf8')).join('\n'));
 
-['dep1.js', 'dep2.js', 'dep3.js', 'x.js'].forEach(fs.unlinkSync);
+['dep1.js', 'dep2.js', 'dep3.js', 'dep4.js', 'x.js'].forEach(fs.unlinkSync);
