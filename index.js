m-ahmadi/tse-client

commit 08a67fc4e6db0d05cf01ac8bfb49da69ed04346f

cli: added `intraday|itd` subcommand
also updated `list` subcommand to reflect new changes

index.js
@@ -8,7 +8,7 @@ const cmd = require('commander');
 const Progress = require('progress');
 const { readFileSync, writeFileSync, existsSync, statSync, mkdirSync } = require('fs');
 const { join, resolve } = require('path');
-const { toGregorian } = require('jalaali-js');
+const { toGregorian, toJalaali } = require('jalaali-js');
 require('./lib/colors.js');
 
 const defaultSettings = {
@@ -23,12 +23,24 @@ const defaultSettings = {
   fileDelimiter:         ',',
   fileEncoding:          'utf8bom',
   fileHeaders:           true,
-  cache:                 true
+  cache:                 true,
+  intraday: {
+    symbols:      [],
+    startDate:    '1d',
+    endDate:      '',
+    gzip:         false,
+    outdir:       './',
+    fileName:     4,
+    fileEncoding: 'utf8bom',
+    fileHeaders:  true,
+    cache:        true
+  }
 };
 if ( !existsSync(join(__dirname,'settings.json')) ) saveSettings(defaultSettings);
 const savedSettings = require('./settings.json');
 const { log } = console;
 const t = '\n\t\t\t\t\t';
+const tt = '\n\t\t\t\t';
 
 cmd
   .helpOption('-h, --help', 'Show help.')
@@ -70,11 +82,33 @@ cmd.command('list').alias('ls').description('Show information about current sett
   .option('-O, --id-sort [columnIndex]',     'Sort the IDs table by specifying the index of the column. default: 1'+t+'put underline at end for ascending sort: 1_')
   .option('--search <query>',                'Search symbols.')
   .action(list);
+
+const itdopts = [
+  '-s, --symbol <string>',
+  '-i, --symbol-file <string>',
+  '-f, --symbol-filter <string>',
+  '-d, --symbol-delete',
+  '-a, --symbol-all',
+  '-b, --start-date <string>',
+  '-o, --outdir <string>',
+  '-n, --file-name <number>',
+  '-e, --file-encoding <string>',
+  '-H, --file-no-headers',
+  '-k, --no-cache'
+].join('\n  ');
+
+cmd.command('intraday [symbols...]').alias('itd').description('Crawl Intraday Data. (help: tse itd -h)')
+  .addHelpText('after', '\nCommon Options:\n  '+itdopts)
+  .option('-m, --end-date <string>',      'Upper boundary for --start-date. default: ""'+tt+'Accepts same patterns as --start-date'+tt+'Cannot be less than --start-date'+tt+'If empty, then latest possible date is used')
+  .option('-z, --gzip',                   'Output raw gzip files. default: false')
+  .option('-y, --alt-date',               'Output results with Shamsi dates. default: false')
+  .action(intraday);
 cmd.parse(process.argv);
 
 const subs = new Set( cmd.commands.map(i=>[i.name(),i.alias()]).reduce((a,c)=>a=a.concat(c),[]) );
 if (cmd.rawArgs.find(i=> subs.has(i))) return;
 if (cmd.opts().cacheDir) { handleCacheDir(cmd.cacheDir); return; }
+
 
 
 (async function () {
@@ -92,7 +126,7 @@ if (cmd.opts().cacheDir) { handleCacheDir(cmd.cacheDir); return; }
   Object.keys(opts).forEach(key => opts[key] === undefined && delete opts[key]);
   
   const settings = { ...defaultSettings, ...savedSettings, ...opts };
-
+  
   log('Total symbols:'.grey, (symbols.length+'').yellow );
 
   if (symbols.length) {
@@ -184,8 +218,156 @@ if (cmd.opts().cacheDir) { handleCacheDir(cmd.cacheDir); return; }
   const { save, saveReset } = rawOpts;
   if (save) saveSettings(settings);
   if (saveReset) saveSettings(defaultSettings);
-  
+
 })();
+
+async function intraday(args, subOpts) {
+  let inserr;
+  const instruments = await tse.getInstruments().catch(err => inserr = err);
+  if (inserr) { log('\nFatal Error #1:  '.red + inserr.title.red +'\n\n'+ inserr.detail.message.red); process.exitCode = 1; return; }
+  const allSymbols = instruments.map(i => i.Symbol);
+  
+  const _defaultSettings = defaultSettings.intraday;
+  const _savedSettings   = savedSettings.intraday;
+  
+  const gOpts = cmd.opts();
+  const symbols = resolveSymbols(allSymbols, savedSettings.symbols, instruments, {args, ...gOpts});
+  
+  const {
+    priceStartDate: startDate,
+    fileOutdir:     outdir,
+    fileName,
+    fileEncoding,
+    fileNoHeaders,
+    cache
+  } = gOpts;
+  const opts = { symbols, startDate, outdir, fileName, fileEncoding, fileHeaders: !fileNoHeaders, cache, ...subOpts };
+  Object.keys(opts).forEach(key => opts[key] === undefined && delete opts[key]);
+  
+  const settings = { ..._defaultSettings, ..._savedSettings, ...opts };
+  
+  log('Total symbols:'.grey, (symbols.length+'').yellow );
+  
+  if (symbols.length) {
+    const progress = new Progress(':bar :percent (Elapsed: :elapsed s)', {total: 100, width: 18, complete: '█', incomplete: '░', clear: true});
+    
+    const { gzip, outdir, cache, fileHeaders, altDate } = settings;
+    let { startDate, endDate, fileName, fileEncoding } = settings;
+    startDate = parseDateOption(startDate);
+    fileName  = +fileName;
+    
+    if (!startDate)                                   { abort('Invalid option:', '--start-date',    '\n\tPattern not matched:'.red, '^\\d{1,3}(y|m|d)$');       return; }
+    if (endDate) {
+      endDate = parseDateOption(endDate);
+      if (!endDate)                                   { abort('Invalid option:', '--end-date',      '\n\tPattern not matched:'.red, '^\\d{1,3}(y|m|d)$');       return; }
+      if (+endDate < +startDate)                      { abort('Invalid option:', '--end-date',      '\n\tCannot be less than'.red, '--start-date');             return; }
+    }
+    if ( !existsSync(outdir) ) mkdirSync(outdir);
+    if ( !statSync(outdir).isDirectory() )            { abort('Invalid option:', '--output-dir',    '\n\tPath is not a directory:'.red,  resolve(outdir).grey); return; }
+    if ( !/^[0-4]$/.test(''+fileName) )               { abort('Invalid option:', '--file-name',     '\n\tPattern not matched:'.red, '^[0-4]$');                 return; }
+    if ( !/^(utf8(bom)?|ascii)$/.test(fileEncoding) ) { abort('Invalid option:', '--file-encoding', '\n\tPattern not matched:'.red, '^(utf8(bom)?|ascii)$');    return; }
+    
+    const _settings = {
+      startDate,
+      endDate,
+      cache,
+      gzip,
+      onprogress:    (n) => progress.tick(n - progress.curr),
+      progressTotal: 86
+    };
+    const { error, data } = await tse.getIntraday(symbols, _settings);
+    
+    if (error) {
+      const { code, title } = error;
+      const fatal = ('\nFatal Error #'+code+':').red +'  '+ title.red +'\n\n';
+      
+      if (code === 1) {
+        const { detail } = error;
+        const msg = typeof detail === 'object' ? detail.message : detail;
+        log(fatal + msg.red);
+      } else if (code === 2) {
+        const { symbols } = error;
+        log(fatal + symbols.join('\n').red);
+      } else if (code === 3 || code === 4) {
+        const { fails, succs } = error;
+        const msg = ''
+          + ('\n'+title+':').redBold + '\n\t'
+          + ('X fail: '+fails.length).red + '\n\t'
+          + ('√ done: '+succs.length).green;
+        log(msg);
+      }
+      process.exitCode = 1;
+      return;
+    }
+    
+    const symins = await tse.getInstruments(true, false, 'Symbol');
+    let bom = '';
+    if (fileEncoding === 'utf8bom') {
+      bom = '\ufeff';
+      fileEncoding = undefined;
+    }
+    
+    const datalen = data.length;
+    const groupCols = tse.itdGroupCols;
+    const filenames = groupCols.map(i => i[0]);
+    
+    const shamsi = s => {
+      const { jy, jm, jd } = toJalaali(+s.slice(0,4), +s.slice(4,6), +s.slice(6,8));
+      return (jy*10000) + (jm*100) + jd + '';
+    };
+    
+    data.forEach((item, i) => {
+      const sym = symbols[i];
+      const instrument = symins[sym];
+      const name = safeWinFilename( getFilename(fileName, instrument) );
+      const dir = join(outdir, name);
+      if ( !existsSync(dir) ) mkdirSync(dir);
+      
+      if (gzip) {
+      
+        for (let [deven, content] of item) {
+          if (altDate) deven = shamsi(''+deven);
+          writeFileSync(join(dir, ''+deven+'.csv.gz'), content);
+        }
+        
+      } else {
+        
+        for (let [deven, content] of item) {
+          if (altDate) deven = shamsi(''+deven);
+          const idir = join(dir, ''+deven);
+          if ( !existsSync(idir) ) mkdirSync(idir);
+          
+          content.split('\n\n').forEach((v,j) => {
+            const headers = fileHeaders ? groupCols[j][1].join() + '\n' : '';
+            writeFileSync(join(idir, filenames[j] + '.csv'), bom+headers+v, fileEncoding);
+          });
+        }
+      
+      }
+      
+      progress.tick(14/datalen);
+    });
+    
+    
+    if (!progress.complete) progress.tick(progress.total - progress.curr);
+    log(' √'.green);
+  } else {
+    log('\nNo symbols to process.'.redBold);
+  }
+  
+  const { save, saveReset } = gOpts;
+  
+  if (save) {
+    savedSettings.intraday = settings;
+    saveSettings(savedSettings);
+  }
+  
+  if (saveReset) {
+    savedSettings.intraday = _defaultSettings;
+    saveSettings(savedSettings);
+  }
+}
+
 function resolveSymbols(allSymbols, savedSymbols=[], instruments, { args, symbol, symbolFile, symbolFilter, symbolDelete, symbolAll }) {
   if (symbolAll) return symbolDelete ? [] : allSymbols;
   
@@ -414,17 +596,32 @@ async function list(opts) {
     const selins = savedSettings.symbols.join('\n');
     log('\nSaved symbols:'.yellow);
     table( selins.length ? selins.yellowBold : 'none'.yellow );
+    
+    const selins2 = savedSettings.intraday.symbols.join('\n');
+    log('\nSaved intraday symbols:'.yellow);
+    table( selins2.length ? selins2.yellowBold : 'none'.yellow );
   }
   
   if (_savedSettings) {
     log('\nSaved settings:'.yellow);
-    const t = {...savedSettings};
-    delete t.symbols;
-    const o = {};
-    Object.keys(t).forEach(k => o[ '--'+k.replace(/([A-Z])/g, '-$1').toLowerCase() ] = t[k]);
-    table(o);
-    // const a = Object.keys(t).reduce((a,k)=> (a.push(['--'+k.replace(/([A-Z])/g, '-$1').toLowerCase(), t[k]]), a), []);
-    // printTable(a);
+    const a = {...savedSettings};
+    const b = a.intraday;
+    
+    delete a.symbols;
+    delete a.intraday;
+    
+    delete b.symbols;
+    
+    const [x, y] = [a, b].map(o => 
+      Object.keys(o).reduce((r, k) => (r[ '--'+k.replace(/([A-Z])/g, '-$1').toLowerCase() ] = o[k], r), {})
+    );
+    
+    table(x);
+    // const x1 = Object.keys(x).reduce((r,k)=> (r.push(['--'+k.replace(/([A-Z])/g, '-$1').toLowerCase(), x[k]]), r), []);
+    // printTable(x1);
+    
+    log('\nSaved intraday settings:'.yellow);
+    table(y);
   }
   
   if (allColumns) {

commit ab211b4886d7eff75aef78f49a58107ca12d991d

cli itd: fix error handling mistake
mistake:
assuming type of `result.error.fails` is the same on code 3 and 4 errors
(introduced in 08a67fc and missed again later in b4600a8)

fix:
differentiate between code 3 and 4 errors and handle each one according
to the type of their `result.error.fails` prop

index.js
@@ -314,14 +314,24 @@ async function intraday(args, subOpts) {
         return;
         
       } else if (code === 3 || code === 4) {
-          const { fails } = error;
-          
+        const { fails } = error;
+        
+        if (code === 3) {
           incompleteCount = fails.length;
           incompleteError = ''
-            + ('\n'+title+':').redBold + '\n'
-            + fails.join('\n').red;
+              + ('\n'+title+':').redBold + '\n'
+              + fails.join('\n').red;
           
           fails.forEach(i => data[ symbols.indexOf(i) ] = undefined);
+          
+        } else if (code === 4) {
+          const syms = Object.keys(fails);
+          
+          incompleteCount = syms.length;
+          incompleteError = ''
+            + ('\n'+title+':').redBold + '\n'
+            + syms.map(sym => sym +': '+ fails[sym].join(' ')).join('\n').red;
+        }
       }
     }
     