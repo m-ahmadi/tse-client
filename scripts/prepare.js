81fc2e05add22b68b149afe065ebb8345675b48d
m-ahmadi <ahmdee18@gmail.com>
2021-05-01 03:11:12 +0430

    scripts/prepare: remove manual browserifying
    of `jalaali-js` since unneeded as of its v1.1.5 from prev bee6e1e

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -4,9 +4,7 @@ process.env.path += require('path').delimiter + './node_modules/.bin';
 
 execSync('terser node_modules/big.js/big.js -o dep1.js -c -m');
 
-fs.writeFileSync('x.js', "module.exports = require('jalaali-js');");
-execSync('browserify x.js -s jalaali | terser -c -m -o dep2.js');
-
+fs.copyFileSync('node_modules/jalaali-js/dist/jalaali.min.js', 'dep2.js');
 fs.copyFileSync('node_modules/localforage/dist/localforage.min.js', 'dep3.js');
 fs.copyFileSync('node_modules/pako/dist/pako.min.js', 'dep4.js');
 
@@ -15,4 +13,4 @@ fs.copyFileSync('tse.js', 'dist/tse.js');
 execSync('terser tse.js -o dist/tse.min.js -c -m');
 fs.writeFileSync('dist/tse.bundle.min.js', ['dep1.js','dep2.js','dep3.js','dep4.js','dist/tse.min.js'].map(i=>fs.readFileSync(i,'utf8')).join('\n'));
 
-['dep1.js', 'dep2.js', 'dep3.js', 'dep4.js', 'x.js'].forEach(fs.unlinkSync);
+['dep1.js', 'dep2.js', 'dep3.js', 'dep4.js'].forEach(fs.unlinkSync);
