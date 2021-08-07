ede653a2031fded26ba775030c9c4e36e1d515d3
m-ahmadi <ahmdee18@gmail.com>
2021-08-07 18:55:44 +0430

    tse: don't trim Instrument `CSecVal` & `CSoSecVal`
    structs/Instrument
      do not trim `CSecVal` & `CSoSecVal` fields (undo of 9891688)
    
    info.json
      modified to reflect changes (previously at 38cd350)

--- tse.js
+++ tse.js
@@ -272,7 +272,7 @@ class Instrument {
     const row = _row.split(',');
     if ( ![18,19].includes(row.length) ) throw new Error('Invalid Instrument data!');
     // unspecified ones are all string
-    this.InsCode      = row[0];         // int64 (long)
+    this.InsCode      = row[0];   // int64 (long)
     this.InstrumentID = row[1];
     this.LatinSymbol  = row[2];
     this.LatinName    = row[3];
@@ -280,16 +280,16 @@ class Instrument {
     this.Symbol       = cleanFa(row[5]).trim();
     this.Name         = row[6];
     this.CIsin        = row[7];
-    this.DEven        = row[8];          // int32 (int)
-    this.Flow         = row[9];          // 0,1,2,3,4,5,6,7 بازار byte
-    this.LSoc30       = row[10];         // نام 30 رقمي فارسي شرکت
-    this.CGdSVal      = row[11];         // A,I,O نوع نماد
-    this.CGrValCot    = row[12];         // 00,11,1A,...25 کد گروه نماد
-    this.YMarNSC      = row[13];         // NO,OL,BK,BY,ID,UI کد بازار
-    this.CComVal      = row[14];         // 1,3,4,5,6,7,8,9 کد تابلو
-    this.CSecVal      = row[15].trim();  // []62 کد گروه صنعت
-    this.CSoSecVal    = row[16].trim();  // []177 کد زير گروه صنعت
-    this.YVal         = row[17];         // string نوع نماد
+    this.DEven        = row[8];   // int32 (int)
+    this.Flow         = row[9];   // 0,1,2,3,4,5,6,7 بازار byte
+    this.LSoc30       = row[10];  // نام 30 رقمي فارسي شرکت
+    this.CGdSVal      = row[11];  // A,I,O نوع نماد
+    this.CGrValCot    = row[12];  // 00,11,1A,...25 کد گروه نماد
+    this.YMarNSC      = row[13];  // NO,OL,BK,BY,ID,UI کد بازار
+    this.CComVal      = row[14];  // 1,3,4,5,6,7,8,9 کد تابلو
+    this.CSecVal      = row[15];  // []62 کد گروه صنعت
+    this.CSoSecVal    = row[16];  // []177 کد زير گروه صنعت
+    this.YVal         = row[17];  // string نوع نماد
     if (row[18]) {
       this.SymbolOriginal = cleanFa(row[18]).trim();
     }
