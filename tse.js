m-ahmadi/tse-client

commit ec57202c2fcbc007f2646b65d2ff0d5e5718349b

fixed updateInstruments() `shares` data append bug
cause: getting the wrong type of data from parseShares() & therefore
resulting in calling join() on an array of objects (instead of strings)

updated parseShares() and added `struct` and `structKey` options to
support the needed data types

changed the logic of combining new & old `shares` data from
`appending new shares to old ones` to `updating changed shares`

tse.js
@@ -68,7 +68,7 @@ class ClosingPrice {
 const cols  = ['CompanyCode', 'LatinName', 'Symbol', 'Name', 'Date', 'ShamsiDate', 'PriceFirst', 'PriceMax', 'PriceMin', 'LastPrice', 'ClosingPrice', 'Price', 'Volume', 'Count', 'PriceYesterday'];
 const colsFa = ['کد شرکت', 'نام لاتین', 'نماد', 'نام', 'تاریخ میلادی', 'تاریخ شمسی', 'اولین قیمت', 'بیشترین قیمت', 'کمترین قیمت', 'آخرین قیمت', 'قیمت پایانی', 'ارزش', 'حجم', 'تعداد معاملات', 'قیمت دیروز'];
 class Column {
-	constructor(row=[]) {	
+	constructor(row=[]) { 
 		const len = row.length;
 		if (len > 2 || len < 1) throw new Error('Invalid Column data!');
 		this.name   = cols[ row[0] ];
@@ -127,15 +127,16 @@ function parseInstruments(struct=false, arr=false, structKey='InsCode') {
 	}
 	return instruments;
 }
-function parseShares(arr=false) {
+function parseShares(struct=false, arr=false, structKey='InsCode') {
 	const rows = localStorage.getItem('tse.shares').split(';');
 	const shares = arr ? [] : {};
 	for (const row of rows) {
-		const item = new Share(row);
+		const item = struct ? new Share(row) : row;
 		if (arr) {
 			shares.push(item);
 		} else {
-			shares[ row.split(',', 2)[1] ] = item;
+			const key = struct ? item[structKey] : row.split(',', 2)[1];
+			shares[key] = item;
 		}
 	}
 	return shares;
@@ -275,9 +276,9 @@ async function updateInstruments() {
 		lastId = 0;
 	} else {
 		currentInstruments = parseInstruments();
-		currentShares      = parseShares(true);
+		currentShares      = parseShares();
 		const insDevens = Object.keys(currentInstruments).map( k => parseInt(currentInstruments[k].match(/\b\d{8}\b/)[0]) );
-		const shareIds = currentShares.map( i => parseInt(i.Idn) );
+		const shareIds = Object.keys(currentShares).map( k => parseInt(currentShares[k].split(',',1)[0]) );
 		lastDeven = Math.max.apply(Math, insDevens);
 		lastId    = Math.max.apply(Math, shareIds);
 	}
@@ -304,7 +305,8 @@ async function updateInstruments() {
 	
 	if (shares !== '') {
 		if (currentShares && currentShares.length) {
-			shares = currentShares.concat( shares.split(';') ).join(';');
+			shares.split(';').forEach(i => currentShares[ i.split(',',1)[0] ] = i);
+			shares = Object.keys(currentShares).map(k => currentShares[k]).join(';');
 		}
 		localStorage.setItem('tse.shares', shares);
 	}

commit 814c5adb012de48ce9c171e6ad09d874155b181d

tse: fix bug in appending & storing of `shares`
bug:
assuming `share` rows are unique by `InsCode` (introduced in ec57202),
which was incorrect, and each `InsCode` can have multiple `share` rows

therefore, in previous code, the first update of `shares` is done
without any issue, but upon future updates, all other `share` rows of an
`InsCode` is deleted and only the last one is kept, which invalidates
all of the `adjust()` calculations

so the logic used before ec57202, was the correct logic (which
originated from 55bb073)

parseShares():
  remove `structKey` arg

  when constructing object result, create an array for each unique key
  and populate it with items that belong to the unique key

  change default value of `arr` arg to `false` (return array by default)

updateInstruments():
  reflect parseShares() signutare change, in the following places,
  when constructing `shareIds`,
  when appending to existing data (append new shares to old ones)

getPrices():
  reflect parseShares() signutare change

resolves #4

tse.js
@@ -358,7 +358,7 @@ function parseInstruments(struct=false, arr=false, structKey='InsCode', itd=fals
   }
   return instruments;
 }
-function parseShares(struct=false, arr=false, structKey='InsCode') {
+function parseShares(struct=false, arr=true) {
   let rows = storage.getItem('tse.shares');
   rows = rows ? rows.split('\n') : [];
   const shares = arr ? [] : {};
@@ -367,8 +367,9 @@ function parseShares(struct=false, arr=false, structKey='InsCode') {
     if (arr) {
       shares.push(item);
     } else {
-      const key = struct ? item[structKey] : row.split(',', 2)[1];
-      shares[key] = item;
+      const key = struct ? item.InsCode : row.split(',', 2)[1];
+      if (!shares[key]) shares[key] = [];
+      shares[key].push(item);
     }
   }
   return shares;
@@ -566,7 +567,7 @@ async function updateInstruments() {
     currentInstruments = parseInstruments();
     currentShares      = parseShares();
     const insDevens = Object.keys(currentInstruments).map( k => +currentInstruments[k].split(',',9)[8] );
-    const shareIds  = Object.keys(currentShares).map( k => +currentShares[k].split(',',1)[0] );
+    const shareIds  = currentShares.map(i => +i.split(',',1)[0]);
     lastDeven = Math.max(...insDevens);
     lastId    = Math.max(...shareIds);
   }
@@ -650,9 +651,8 @@ async function updateInstruments() {
   }
   
   if (shares !== '') {
-    if (currentShares && Object.keys(currentShares).length) {
-      shares.split(';').forEach(i => currentShares[ i.split(',',1)[0] ] = i);
-      shares = Object.keys(currentShares).map(k => currentShares[k]).join('\n');
+    if (currentShares && currentShares.length) {
+      shares = currentShares.concat( shares.split(';') ).join('\n');
     } else {
       shares = shares.replace(/;/g, '\n');
     }
@@ -907,7 +907,7 @@ async function getPrices(symbols=[], _settings={}) {
   });
   
   const { adjustPrices, daysWithoutTrade, startDate, csv } = settings;
-  const shares = parseShares(true, true);
+  const shares = parseShares(true);
   const pi = Big(ptot).mul(0.20).div(selection.length);
   
   if (csv) {